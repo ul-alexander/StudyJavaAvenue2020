f56e7e868aa2718f4cae2f48bcd7842569ed7244
Александр <alexander.shalashov@gmail.com>
2020-01-26 22:19:18 +0300

    Lesson 18 Add Log4J Custom log4j

--- src/Lesson18/Main.java
+++ src/Lesson18/Main.java
@@ -1,7 +1,14 @@
 package Lesson18;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    //Инициализация log4j
+    private static final Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.info("Пользователь вошел в админ-панель");
 
     }
 }
